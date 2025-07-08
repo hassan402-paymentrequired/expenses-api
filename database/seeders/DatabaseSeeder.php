ddcd36eaa916db6aa2c893b078d6a10a70da33aa
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-07-08 12:55:25 +0100

    new update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        // \App\Models\User::factory()->count(10)->create();
+        \App\Models\User::factory()->count(5)->create();
         $this->call(TaskSeeder::class);
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@gmailil.com',
+        // ]);
     }
 }
